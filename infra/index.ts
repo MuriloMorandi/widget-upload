9245690de8773be2ed5fb19d8f7213a1a141bbb1
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2025-08-16 16:38:22 -0300

    test: Criando um novo bucket s3

--- infra/index.ts
+++ infra/index.ts
@@ -1,8 +1,15 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.Bucket("widget-upload-iac-bucket", {
+const bucket_primeiro = new aws.s3.Bucket("widget-upload-iac-bucket", {
     bucket: 'primeiro-teste',
+    tags: {
+        IAC: 'true'
+    }
+});
+
+const bucket_segundo = new aws.s3.Bucket("widget-upload-iac-bucket", {
+    bucket: 'segundo-teste',
     tags: {
         IAC: 'true'
     }
@@ -17,9 +24,13 @@ const ecr = new aws.ecr.Repository('widget-upload-iac-ecr', {
 });
 
 
-export const bucketName = bucket.id;
-export const bucketInfo = bucket.bucket;
-export const bucketArn = bucket.arn;
+export const bucketPrimeiroName = bucket_primeiro.id;
+export const bucketPrimeiroInfo = bucket_primeiro.bucket;
+export const bucketPrimeiroArn = bucket_primeiro.arn;
+
+export const bucketSegundoName = bucket_segundo.id;
+export const bucketSegundoInfo = bucket_segundo.bucket;
+export const bucketSegundoArn = bucket_segundo.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepository = ecr.repositoryUrl;
