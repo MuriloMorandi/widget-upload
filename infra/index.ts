7a9b0ea3bfa1608a51e7ffb705c6acf949f78565
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2025-08-22 01:01:51 -0300

    Test preview iac

--- infra/index.ts
+++ infra/index.ts
@@ -15,6 +15,14 @@ const ecr = new aws.ecr.Repository('widget-upload-iac-ecr', {
     }
 });
 
+const ecr2 = new aws.ecr.Repository('widget-upload-iac-ecr2', {
+    name: 'widget-upload-iac-ecr2',
+    imageTagMutability: 'IMMUTABLE',
+    tags: {
+        IAC: 'true'
+    }
+});
+
 
 export const bucketName = bucket.id;
 export const bucketInfo = bucket.bucket;
@@ -22,3 +30,6 @@ export const bucketArn = bucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepository = ecr.repositoryUrl;
+
+export const ecrName2 = ecr2.name;
+export const ecrRepository2 = ecr2.repositoryUrl;
